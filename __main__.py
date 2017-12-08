2cd6966849fffd2d36feefe923f53ffebcd8ecd2
Ghoul Nadir <nadir.ghoul@u-psud.fr>
2017-12-08 05:40:26 +0100

    commit

--- __main__.py
+++ __main__.py
@@ -1,10 +1,10 @@
 from slackclient import SlackClient
-
+import requests, json
 
 BOT_NAME = 'bobot'
-BOT_KEY = 'xoxb-283548997155-U0hxkV8BdvQCRslWpdaiz4jy'
+BOT_TOKEN = 'xoxb-283548997155-U0hxkV8BdvQCRslWpdaiz4jy'
 
-slack_client = SlackClient(BOT_KEY)
+slack_client = SlackClient(BOT_TOKEN)
 
 
 if __name__ == "__main__":
@@ -12,25 +12,36 @@ if __name__ == "__main__":
     if api_call.get('ok'):
         # retrieve all users so we can find our bot
         users = api_call.get('members')
-        for user in users:
-            if 'name' in user and user.get('name') == BOT_NAME:
-                print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
-                '''slack_client.api_call(
-                    "chat.postMessage",
-                    channel="#general",
-                    text="caca #maturité",
-                    user=user.get('id')
-                )'''
-                # Get every channels
-                '''for i in slack_client.api_call(
-                    "conversations.list",
-                    types="public_channel, private_channel"
-                )['channels']:
-                    if i['name'] == 'general':'''
-                print(slack_client.api_call(
-                    method='chat.message',
-                    channel='#general'
-                ))
+        user = None
+        for u in users:
+            if 'name' in u and u.get('name') == BOT_NAME:
+                print("Bot ID for '" + u['name'] + "' is " + u.get('id'))
+                user = u
+                # Get every channels"""
+        for i in slack_client.api_call(
+            "conversations.list",
+            types="public_channel, private_channel"
+        )['channels']:
+            if i['name'] == 'general':
+                while True:
+                    u = slack_client.api_call("channels.info", channel=i['id'])
+                    if 'channel' in u:
+                        message = u['channel']['latest']
+                        if message['type'] == 'message':
+                            # If the message was sent by a humain
+                            if 'user' in message:
+                                print(message)
+                                question = message['text']
+                                answer = json.loads(requests.post(
+                                    'http://ndi.eliott.tech/api/answer/',
+                                    data = dict(question=question)
+                                ).text)['answer']
+                                slack_client.api_call(
+                                    "chat.postMessage",
+                                    channel="#general",
+                                    text=answer,
+                                    user=user.get('id')
+                                )
 
     else:
         print("could not find bot user with the name " + BOT_NAME)
